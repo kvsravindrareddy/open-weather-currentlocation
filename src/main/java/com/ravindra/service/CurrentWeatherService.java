77eb3bd1a0219ea7e019d9e2b41b93d5a16bec14
Kakarla V S Ravindra Reddy <kvsravindrareddy@gmail.com>
2019-09-15 23:42:24 +0530

    Open Weather API Fixes

--- src/main/java/com/ravindra/service/CurrentWeatherService.java
+++ src/main/java/com/ravindra/service/CurrentWeatherService.java
@@ -6,8 +6,8 @@ import javax.naming.ServiceUnavailableException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -54,20 +54,33 @@ public class CurrentWeatherService {
 	 */
 	public CurrentWeather getCurrentWhether(String city) throws ServiceUnavailableException, IOException {
 		log.info("Begin getCurrentWhether()-CurrentWeatherService");
-		CurrentWeather currentWeather = new CurrentWeather();
-		currentWeather.setCityName(city);
+		CloseableHttpClient client = HttpClientBuilder.create().build();
 		String serviceUrl = uriBuilder(city);
-		String weatherResponse = restTemplate.getForObject(serviceUrl, String.class);
-		JSONObject responseJson = new JSONObject(weatherResponse);
-		JSONArray weatherJsonArr = responseJson.getJSONArray("weather");
-		currentWeather.setWeatherDesc(weatherJsonArr.getJSONObject(0).getString("description"));
-		JSONObject mainObj = responseJson.getJSONObject("main");
-		currentWeather.setCurrTemp(mainObj.getDouble("temp"));
-		currentWeather.setTempMin(mainObj.getDouble("temp_min"));
-		currentWeather.setTempMax(mainObj.getDouble("temp_max"));
-		JSONObject sysObj = responseJson.getJSONObject("sys");
-		currentWeather.setSunrise(sysObj.getBigInteger("sunrise"));
-		currentWeather.setSunset(sysObj.getBigInteger("sunset"));
+		log.debug("Weather serivce URL : " + serviceUrl);
+		HttpResponse response = client.execute(new HttpGet(serviceUrl));
+		int statusCode = 0;
+		statusCode = response.getStatusLine().getStatusCode();
+		log.debug("Weather service status code " + statusCode);
+		CurrentWeather currentWeather = null;
+		if (statusCode == 200) {
+			currentWeather = new CurrentWeather();
+			currentWeather.setCityName(city);
+			String weatherResponse = restTemplate.getForObject(serviceUrl, String.class);
+			JSONObject responseJson = new JSONObject(weatherResponse);
+			JSONArray weatherJsonArr = responseJson.getJSONArray("weather");
+			currentWeather.setWeatherDesc(weatherJsonArr.getJSONObject(0).getString("description"));
+			JSONObject mainObj = responseJson.getJSONObject("main");
+			currentWeather.setCurrTemp(convertKelvinToCelsius(mainObj.getDouble("temp")));
+			currentWeather.setTempMin(convertKelvinToCelsius(mainObj.getDouble("temp_min")));
+			currentWeather.setTempMax(convertKelvinToCelsius(mainObj.getDouble("temp_max")));
+			JSONObject sysObj = responseJson.getJSONObject("sys");
+			currentWeather.setSunrise(sysObj.getBigInteger("sunrise"));
+			currentWeather.setSunset(sysObj.getBigInteger("sunset"));
+		} else {
+			log.error("ServiceNotFoundException occured in getCurrentWhether " + statusCode);
+			throw new ServiceUnavailableException(
+					"Open Weather API Service is not available at this time. Please try again after sometime.");
+		}
 		log.info("End getCurrentWhether()-CurrentWeatherService");
 		return currentWeather;
 	}
@@ -84,21 +97,19 @@ public class CurrentWeatherService {
 	 */
 	private String uriBuilder(String currentCity) throws ServiceUnavailableException, IOException {
 		log.info("Begin uriBuilder()-CurrentWeatherService");
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpResponse response = client.execute(new HttpGet(currentWhetherUrl));
-		int statusCode = 0;
-		statusCode = response.getStatusLine().getStatusCode();
-		log.debug("Weather service status code : " + statusCode);
-		String serviceUrl = "";
-		if (statusCode == 200) {
-			serviceUrl = UriComponentsBuilder.fromHttpUrl(currentWhetherUrl).queryParam("q", currentCity)
-					.queryParam("APPID", subscriptionkey).build().toUri().toString();
-		} else {
-			log.error("ServiceNotFoundException occured in getCurrentWhether " + statusCode);
-			throw new ServiceUnavailableException(
-					"Open Weather API Service is not available at this time. Please try again after sometime.");
-		}
-		return serviceUrl;
+		return UriComponentsBuilder.fromHttpUrl(currentWhetherUrl).queryParam("q", currentCity)
+				.queryParam("APPID", subscriptionkey).build().toUri().toString();
+	}
+
+	/**
+	 * Convert Kelvin temparature which is returned by Open Weather API and convert
+	 * it to Celsius
+	 * 
+	 * @param kelvin
+	 * @return
+	 */
+	private Double convertKelvinToCelsius(Double kelvin) {
+		return kelvin - 273.15;
 	}
 
 	/**
